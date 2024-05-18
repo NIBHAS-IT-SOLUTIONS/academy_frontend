95bbf049c3579ad4ffdc1e64919698447d322c2f
nibhas <nibhasitsolutions@gmail.com>
2024-05-18 09:46:38 +0530

    design update

---
+++ src/components/Landing page/Landing.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Landing.css'
+
+function Landing() {
+    return (
+        <>
+            <div class="flex-container">
+                <div className="sub-container">
+                    <div class="flex-one-left">
+                        <div className="round1"></div>
+                        <div className="round2"></div>
+                        <div className="round3"></div>
+
+
+                        <h1 className='land-heading'>The Best Way For <br /><span>Your Learning</span></h1>
+                    </div>
+                    <div className="flex-one-right">
+                        <img src="https://i.postimg.cc/KvG26ZhM/Saly-16.png" alt="" />
+                    </div>
+                </div>
+
+                <div class="flex-two">2</div>
+            </div>
+        </>
+    )
+}
+
+export default Landing
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,38 +4,21 @@ import Footer from '../components/Footer/Footer'
 import axios from 'axios'
 import { baseURL } from '../constants/constants'
 import Enrolls from '../components/Enrolls/Enrolls'
+import Landing from '../components/Landing page/Landing'
 
 function Home() {
   // eslint-disable-next-line
-  const [formData, setFormData] = useState({
-    name: 'gh',
-    age: 'sd',
-    city: 'sd'
-  });
-  // eslint-disable-next-line
-  const handleSession=async()=>{
-    var token=localStorage.getItem('name')
-    const headers = {
-      'Content-Type': 'application/json',
-      'token':token
-    };
-
-    try{
-      var res=await axios.post(`${baseURL}/session_auth/`,{formData},{headers})
-      console.log(res.data);
-    }
-    catch(err){
-      console.log(err);
-    }
-  }
+ 
   return (
-    <div>
-        <Navigation/>
-       
-        <br /><br /><br /> {/*
+    <>
+      <Navigation />
+
+      <br /><br /><br /> {/*
         <input type='button' value="authntictn" onClick={handleSession} /> */}
-       <Enrolls/>
-    </div>
+        <Landing/>
+   
+
+    </>
   )
 }
 
